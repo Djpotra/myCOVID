901bc2e4bb1bfb5db3a9716c1f4b40df7a340878
dch <dch980201@gmail.com>
2022-08-24 11:34:23 +0000

    ..

---
+++ myCOVID-back/utils/getData.js
@@ -0,0 +1,27 @@
+let axios = require('axios');
+
+
+
+async function getData(url) {
+    let data = await axios.request(url)
+        .then(res => res.data.replaceAll('\n',''));
+    let scripts = await data.match(/<body>.*<\/body>/)
+    .reduce((cur,next)=>{
+        return [...cur,...next.split(/<noscript.*>.*<\/noscript>/g)];
+    },[])
+    .reduce((cur,next)=>{
+        return [...cur,...next.match(/<script.*>.*?<\/script>/)];
+    },[])
+    .reduce((cur,next)=>{
+        return [...cur,...next.split(/<\/script>/g)];
+    },[]);
+
+    scripts = scripts.map(s=>s.replace(/<script.*>/g,'')).filter(s=>s.length>0);
+
+    let window = {};
+    scripts.forEach(s=>eval(s));
+    
+    return window;
+}
+
+module.exports = getData;
